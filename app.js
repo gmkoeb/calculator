162ed20b6dacc041b2f5bbf04663bb14d36b0484
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-02-07 15:38:30 -0300

    Included form and submit with post method to a
    math api for desired operation

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const app = express()
+const https = require("https");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/views/index.html")
+})
+
+
+app.post('/', function(req, res){
+    let query = req.body.operation
+    console.log(query)
+    let url = "https://api.mathjs.org/v4/?" + "expr=" + encodeURIComponent(query)    
+    console.log(url)
+    https.get(url, function (response) {
+        response.on("data", function (data) {
+          let answer = JSON.parse(data);
+          document.body.querySelector("input").value = query + answer
+        });
+      });    
 })
 
 app.listen(3000, function(req, res){
